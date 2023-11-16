fa8dde2979cb29d7372786eca86b9b4096351483
Garcia <garcia@moov.life>
2023-11-16 16:48:01 +0200

    Initial Commit

---
+++ e-commerce/babies/api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from .views import home
+
+urlpatterns = [
+    path('', home),
+    path('category', include('api.category.urls'))
+]
