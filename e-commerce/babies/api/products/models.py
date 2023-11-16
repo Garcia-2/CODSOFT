d57085e320ce572aaa439bdf5835510bd74613f4
Garcia <garcia@moov.life>
2023-11-16 20:16:04 +0200

    Image serializer and product id

--- e-commerce/babies/api/products/models.py
+++ e-commerce/babies/api/products/models.py
@@ -3,6 +3,7 @@ from api.category.models import Category
 
 # Create your models here.
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
--- e-commerce/babies/api/products/serializers.py
+++ e-commerce/babies/api/products/serializers.py
@@ -2,9 +2,11 @@ from .models import Product
 from rest_framework import serializers
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    image = serializers.ImageField(max_length=None, allow_empty_file=False, allow_null=False, use_url=True, required=False)
     class Meta:
         model = Product
-        fields = {
-            "name", "description", "prize", "stock", 
-        }
-        fields = '__all__'
+        fields = (
+            "id","name", "description", "price", "stock",
+            "stock", "image", "category"
+        )
+        # fields = '__all__'
