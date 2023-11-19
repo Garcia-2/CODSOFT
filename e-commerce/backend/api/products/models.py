9d1383fdafbc68df21b2f307a94bb56c1130f300
Garcia <garcia@moov.life>
2023-11-19 13:59:23 +0200

    migrations

--- e-commerce/backend/api/products/models.py
+++ e-commerce/backend/api/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from api.category.models import Category
 from api.user.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Product(models.Model):
@@ -14,7 +15,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.name}"
--- e-commerce/backend/api/user/models.py
+++ e-commerce/backend/api/user/models.py
@@ -7,3 +7,9 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return f"Username:{self.username}. Email:{self.email}. User Id:{self.id}"
+
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
+
+User._meta.get_field('groups').remote_field.related_name = 'api_user_groups'
+User._meta.get_field('user_permissions').remote_field.related_name = 'api_user_permissions'
